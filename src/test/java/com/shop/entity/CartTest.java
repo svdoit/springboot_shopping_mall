b5508cd720c0d7fa3a7982c7651c4c5ec4490ded
svdoit <dododo@ibk.co.kr>
2023-05-12 15:43:56 +0900

    [Add] Cascade ~208pg

---
+++ src/test/java/com/shop/entity/CartTest.java
@@ -0,0 +1,41 @@
+/*
+package com.shop.entity;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.repository.CartRepository;
+import com.shop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class CartTest {
+    @Autowired
+    CartRepository cartRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @PersistenceContext
+    EntityManager em;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+}
+*/
